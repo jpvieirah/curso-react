dd38f2317c3d07f3d23a6d590a0c261905c8a474
jpvieira1 <jp201101@gmail.com>
2022-03-16 10:09:51 -0300

    Primeiro Componente #01

--- fundamentos-react/src/index.js
+++ fundamentos-react/src/index.js
@@ -2,12 +2,14 @@ import './index.css'
 import ReactDOM from 'react-dom'
 import React from 'react'
 
+import Primeiro from './components/basicos/Primeiro'
 
-const tag =   <strong>Olá React!!</strong>
 
 ReactDOM.render(
     <div>
-        {tag}
-        </div>,
+        
+        <Primeiro></Primeiro>
+
+    </div>,
     document.getElementById('root')
     )
