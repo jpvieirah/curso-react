da0a280f392934a36a99fc775feab1af3120394c
jpvieira1 <jp201101@gmail.com>
2022-03-16 11:07:52 -0300

    Propriedades são Somente Leitura

--- fundamentos-react/src/components/basicos/ComParametro.jsx
+++ fundamentos-react/src/components/basicos/ComParametro.jsx
@@ -3,6 +3,8 @@ import React from "react"
 export default function ComParametro(props) {
     const status = props.nota >= 7 ? 'Aprovado' : 'Reprovado'
     const notaInt = Math.ceil(props.nota)
+    /*caso precise alterar o valor de uma propriedaede 
+    declare uma nova variavel com as condições conforme realizado acima*/ 
 
     return (
 
