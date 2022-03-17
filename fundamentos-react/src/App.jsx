ca9c81557607d183be3f34b6fc0c9ffcbca2ea9d
jpvieira1 <jp201101@gmail.com>
2022-03-17 10:02:45 -0300

    Componente Card #03

--- fundamentos-react/src/App.jsx
+++ fundamentos-react/src/App.jsx
@@ -12,19 +12,19 @@ export default (_) => (
     <h1>Fundamentos React </h1>
 
     <div className="Cards">
-      <Card titulo="#04 Desafio Aleátorio">
+      <Card titulo="#04 Desafio Aleátorio" color="#7fffd4" >
         <Aleatorio min={1} max={60} />
       </Card>
 
-      <Card titulo="#03 Fragmento">
+      <Card titulo="#03 Fragmento" color="#E94C6F">
         <Fragmento />
       </Card>
 
-      <Card titulo="#02 Com Paramêtro">
+      <Card titulo="#02 Com Paramêtro" color="#E8B71A">
         <ComParametro titulo="Situação do aluno" aluno="Pedro" nota={7.6} />
       </Card>
 
-      <Card titulo="#01 Primeiro Componente">
+      <Card titulo="#01 Primeiro Componente" color="#588C73">
         <Primeiro></Primeiro>
       </Card>
     </div>
