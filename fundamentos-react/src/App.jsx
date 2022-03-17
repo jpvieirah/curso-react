5b33901f30938babc0a9aad9e49d8698f3ef6e35
jpvieira1 <jp201101@gmail.com>
2022-03-17 09:39:45 -0300

    Componente Card #02

--- fundamentos-react/src/App.jsx
+++ fundamentos-react/src/App.jsx
@@ -1,38 +1,32 @@
+import "./App.css"
 import React from "react";
 
 import Card from "./components/layout/Card";
 import Aleatorio from "./components/basicos/Aleatorio";
-import Fragmento from './components/basicos/Fragmento';
-import ComParametro from './components/basicos/ComParametro';
-import Primeiro from './components/basicos/Primeiro';
+import Fragmento from "./components/basicos/Fragmento";
+import ComParametro from "./components/basicos/ComParametro";
+import Primeiro from "./components/basicos/Primeiro";
 
 export default (_) => (
-     <div id="app">
-            <h1>Fundamentos React </h1>
+  <div className="App">
+    <h1>Fundamentos React </h1>
 
-        <Card titulo="#04 Desafio Aleátorio">
+    <div className="Cards">
+      <Card titulo="#04 Desafio Aleátorio">
         <Aleatorio min={1} max={60} />
-            </Card>
+      </Card>
 
-            <Card titulo="#03 Fragmento">
-            <Fragmento />
-            </Card>
+      <Card titulo="#03 Fragmento">
+        <Fragmento />
+      </Card>
 
-            <Card titulo="#02 Com Paramêtro">
-            <ComParametro 
-        titulo="Situação do aluno" 
-        aluno="Pedro" 
-        nota={7.6}/>
-            </Card>
+      <Card titulo="#02 Com Paramêtro">
+        <ComParametro titulo="Situação do aluno" aluno="Pedro" nota={7.6} />
+      </Card>
 
-            <Card titulo="#01 Primeiro Componente">
-            <Primeiro></Primeiro>
-            </Card>
-
-    
-           
-   
-        
+      <Card titulo="#01 Primeiro Componente">
+        <Primeiro></Primeiro>
+      </Card>
     </div>
-
+  </div>
 );
