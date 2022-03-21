29b4caad4b247deddcb5318acfe6955363dad700
helsin <jp201101@gmail.com>
2022-03-21 12:07:22 -0300

    Contador #02

--- fundamentos-react/src/components/contador/Contador.jsx
+++ fundamentos-react/src/components/contador/Contador.jsx
@@ -1,6 +1,10 @@
 import "./Contador.css";
 import React, {Component} from "react";
 
+import Display from "./Display";
+import Botoes from "./Botoes";
+import PassoForm from "./PassoForm";
+
  class Contador extends Component {
 
     state = {
@@ -30,10 +34,10 @@ import React, {Component} from "react";
 
     }
 
-    setPasso = (e) => {
+    setPasso = (novoPasso) => {
 
         this.setState({
-            passo: +e.target.value,
+            passo: novoPasso,
 
         })
 
@@ -46,15 +50,9 @@ import React, {Component} from "react";
 
                 <div className="Contador">
                     <h2>Contador</h2>
-                    <h3>{this.state.numero}</h3>
-                    <div>
-                        <label htmlFor="passoInput">Passo: </label>
-                        <input id="passoInput" type="number"
-                        value={this.state.passo}
-                         onChange={this.setPasso}/>
-                    </div>
-                <button onClick={this.inc}>+</button>
-                <button onClick={this.dec}>-</button>
+                    <Display numero={this.state.numero}/>
+                    <PassoForm passo={this.state.passo} setPasso={this.setPasso}/>
+                    <Botoes setInc={this.inc} setDec={this.dec}/>
                 </div>
 
             )
